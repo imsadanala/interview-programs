b664cb790d3171673edd9012b6a40e31df64aa84
Suresh Sadanala <sadanalansp@gmail.com>
2021-03-15 14:11:25 +0530

    OneEditAway changes

---
+++ Interview-Programs/src/com/interview/Test.java
@@ -0,0 +1,13 @@
+package com.interview;
+
+import com.interview.programs.gaint.org.Programs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Programs.oneEditAway("live", "leave"));
+		
+	}      
+
+}
---
+++ Interview-Programs/src/com/interview/programs/gaint/org/Programs.java
@@ -0,0 +1,54 @@
+package com.interview.programs.gaint.org;
+
+/*
+ * 
+ */
+public class Programs {
+	
+	
+	/**
+	 * Consider 2 strings b and a. write a code snippet that determines whether we can obtain 2 identical strings
+	 * by performing single edit b/a. More precisely, we can insert, delete or replace by single character in "b" or in "a", and "b" will become equal to "a"
+	 *  Examples:
+	 *  live, love  -> one edit as "o" can replace by "i" or vice versa
+	 *  lve, live   -> one edit, insert in "i" in "lve" or remove "i" from "live"
+	 *  live, leave -> more than one edit
+	 *  lie, lines  -> more than one edit   
+	 *  Ref: https://youtu.be/5FOWy_2tzck
+	 *
+	 */
+	public static boolean oneEditAway(String a, String b) {
+
+		if (Math.abs(a.length() - b.length()) > 1) {
+			return false;
+		}
+
+		String shorterStr = a.length() < b.length() ? a : b;
+		String longerStr = a.length() > b.length() ? a : b;
+
+		int sIndex = 0;
+		int lIndex = 0;
+		int mismatchCount = Math.abs(a.length() - b.length());
+
+		while (sIndex < shorterStr.length() || lIndex < longerStr.length()) {
+			if (shorterStr.charAt(sIndex) != longerStr.charAt(lIndex)) {
+				mismatchCount++;
+			}
+			if (mismatchCount > 1) {
+				return false;
+			}
+			
+			if(shorterStr.length() == longerStr.length()) {
+				sIndex++;
+			} else {
+				sIndex++;
+			}
+			
+			lIndex++;
+		}
+
+		return true;
+	}
+
+	
+}
